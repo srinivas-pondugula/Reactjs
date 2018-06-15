fb7bbf407a3fd6ae95f0f95388e5b70f9f87f50d
srinivas-pondugula <spondugula@silver-peak.com>
2018-06-14 20:03:34 -0700

    Add files via upload

---
+++ ShoppginList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class ShoppginList extends React.Component {
+	render(){
+		return (
+			<div className="shopping-list">
+				<h1>Shopping List for {this.props.name}</h1>
+				<ul>
+				  <li>Instagram</li>
+				  <li>WhatsApp</li>
+				  <li>Oculus</li>
+				</ul>
+		    </div>
+		);
+	}
+}
+
+
+export default ShoppginList
